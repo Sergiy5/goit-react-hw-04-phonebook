d23efd7045de2d066757b76eabde0ff8b948d5a1
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-05-18 10:43:42 +0300

    Add hooks to form, all dun

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -1,66 +1,61 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid'
 import { WraperForm, Input, BtnSubmit, Label } from './form.styled';
 
-class Form extends Component {
+const Form = ({onSubmit} )=> {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  state = {
-    name: '',
-    number: '',
-    };
-   
-    handleChange = ({target}) => {
-        
-        this.setState({
-        [target.name]: target.value,
-    })
+  const handleChange = ({ target }) => {
+    if (target.name === 'name') {
+      setName(target.value);
     }
-    handleSubmit = (e) => {
-      e.preventDefault()
-      const id = nanoid(5)
-      const {name, number } = this.state
-      this.props.onSubmit({ id, name, number })
-      this.setState({
-    name: '',
-    number: '',
-    })
+    if (target.name === 'number') {
+      setNumber(target.value);
     }
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    const id = nanoid(5);
+    // const {name, number } = this.state
+    onSubmit({ id, name, number });
+   setName('')
+   setNumber('')
+  };
 
-  render() {
-    return (
-      <form onSubmit = {this.handleSubmit}>
-        <WraperForm>
-          <Label htmlFor="nameInput" className="lableInputName">
-            Name
-          </Label>
-            <Input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleChange}
-            value={this.state.name}
-                />
-                <Label htmlFor="numberInput" className="lableInputNumber">
-            Number
-          </Label>
-            <Input
-           type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            onChange={this.handleChange}
-            value={this.state.number}
-          />
-          <BtnSubmit type="submit" className="btnSubmit">
-            Add contact
-          </BtnSubmit>
-        </WraperForm>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <WraperForm>
+        <Label htmlFor="nameInput" className="lableInputName">
+          Name
+        </Label>
+        <Input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleChange}
+          value={name}
+        />
+        <Label htmlFor="numberInput" className="lableInputNumber">
+          Number
+        </Label>
+        <Input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleChange}
+          value={number}
+        />
+        <BtnSubmit type="submit" className="btnSubmit">
+          Add contact
+        </BtnSubmit>
+      </WraperForm>
+    </form>
+  );
+};
 
 export default Form;
